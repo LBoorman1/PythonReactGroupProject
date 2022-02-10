12d692522ee8c919c12bef108a2a7c3370688cec
Max McIntyre <maxmcintyre@gmail.com>
2022-02-10 13:57:53 +0000

    Try to fix issues with frontend folder

--- mentoringsystem/mentoring/models.py
+++ mentoringsystem/mentoring/models.py
@@ -9,3 +9,6 @@ class Meeting(models.Model):
     attendance_status = models.CharField(choices=ATTENDANCE_STATUS_CHOICES, max_length=10)
     title = models.CharField(max_length=20)
     description = models.TextField()
+
+class Relationship(models.Model):
+    pass 
