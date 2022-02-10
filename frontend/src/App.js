83d3716562bf524ec4d4e239858fa188e40fee89
Max McIntyre <maxmcintyre@gmail.com>
2022-02-10 14:18:05 +0000

    Fix issues with frontend folder

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React , { Component } from 'react';
+//import ReactDOM from 'react-dom';
+
+import Header from './layout/Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <h1>Mentoring System</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
