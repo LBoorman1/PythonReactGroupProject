5971767f08d51d52abd43a0ed8ddb4ea653dc473
IsabellaFaye <77329266+IsabellaFaye@users.noreply.github.com>
2022-02-25 15:54:37 +0000

    Isabella (#22)
    * Adding poa checkbox component
    
    * added POA component
    
    * poa add date
    
    * Added session threshold
    
    * set session slider

--- frontend/src/components/CreateAccount.js
+++ frontend/src/components/CreateAccount.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {Button} from 'reactstrap';
 
 const CreateAccount = () => {
       return (
         <div className="create_account sec__one">
           <h1> Create Account </h1>
           <form method="post" >
+          <label>
+                <h4>First Name: </h4> 
+                <input type="text" name="name" />
+                <br></br>
+                <h4>Second Name: </h4>
+                <input type="text" name="name" />
+            </label>
+
+            <br></br>
+
             <table>
               <tr>
                 <td>&nbsp;</td>
--- frontend/src/components/EditDetails.js
+++ frontend/src/components/EditDetails.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import {useState} from 'react';
 
-const EditDetails = () => {
+
+const EditDetails = props => {
+    const [item,setItem]= useState(null);
     return (
         <div className="edit_details sec__one">
             <h1> Edit Details </h1>
+            <h3>Forename: {props.forename}</h3>
+            <h3>Surname: {props.surname}</h3>
+            <h3>Department: {props.department}</h3>
+
+            <input value={item} type="checkbox"/>
+            <input value={item} type="checkbox"/>
+            <input value={item} type="checkbox"/>
+
+
+           
+            
         </div>
     )
 }
